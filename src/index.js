a51fee99a981a3e414365c6948629c42a4f022cd
niro1n <syahfrinorezky@gmail.com>
2026-08-17 03:10:18 +0800

    feat: implement command handling and error management for sajda bot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,71 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits, Events } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { Client, GatewayIntentBits, Events, Collection } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
 
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+
+    console.log(`Loaded command: /${command.data.name}`);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+    );
+  }
+}
+
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-clien.on(Events.Error, (error) => {
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.warn(`Command /${interaction.commandName} not found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(`Error executing /${interaction.commandName}:`, error);
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "Terjadi kesalahan saat menjalankan command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "Terjadi kesalahan saat menjalankan command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+
+client.on(Events.Error, (error) => {
   console.error("The client encountered an error:", error);
 });
 
