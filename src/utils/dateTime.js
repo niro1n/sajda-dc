72adb72dc9266a8fec24497408fbfc5258b8363b
niro1n <syahfrinorezky@gmail.com>
2026-08-20 10:54:37 +0800

    feat: add getCurrentDateTime function for formatted date and time retrieval

--- src/utils/dateTime.js
+++ src/utils/dateTime.js
@@ -8,6 +8,31 @@ function getCurrentDate() {
   return `${day}-${month}-${year}`;
 }
 
+function getCurrentDateTime(timezone) {
+  const now = new Date();
+
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    timeZone: timezone,
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  const parts = formatter.formatToParts(now);
+
+  const get = (type) => parts.find((part) => part.type === type).value;
+
+  return {
+    date: `${get("day")}-${get("month")}-${get("year")}`,
+    hour: Number(get("hour")),
+    minute: Number(get("minute")),
+  };
+}
+
 module.exports = {
   getCurrentDate,
+  getCurrentDateTime,
 };
